a94859181e8b8f513f4d4bae18471b8c8538421b
Abdul Razzaque Jawad <Abdul.Razzaque@visionetsystems.com>
2023-07-23 20:52:28 +0500

    Add simple microservices example

---
+++ README.md
@@ -0,0 +1,8 @@
+You can use `npm run start:dev` command in each service to start the server
+
+My employee number is 18511, so I will be running these services on following ports
+
+- inventory-service (5001)
+- shipping-service (5008)
+- users-service (5005)
+- billing-service (5009)
---
+++ billing-service/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+
+// init express app
+const app = express();
+
+// use morgan middleware
+app.use(morgan("combined"));
+app.use(express.json());
+
+app.post("/billing", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(3000);
